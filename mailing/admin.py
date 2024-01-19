9d5980e6051794c32c73b6931f58f671f7f157bf
Alexander <83513963+illustratorOut@users.noreply.github.com>
2024-01-20 00:51:01 +0300

    Delete mailing directory

--- mailing/admin.py
+++
@@ -1,9 +0,0 @@
-from django.contrib import admin
-
-from mailing.models import MessageMailing
-
-
-@admin.register(MessageMailing)
-class MessageMailingAdmin(admin.ModelAdmin):
-    list_display = ('topic', 'body')
-    list_filter = ('mailing',)
